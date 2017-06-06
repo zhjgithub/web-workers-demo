5ec6b36bff936d08a2cab864a1c8282ee20ae148
organism <neuqzhj@live.com>
2017-06-06 18:41:21 +0800

    feature: Use Web Server and Transferable objects

--- scripts/imageManips.js
+++ scripts/imageManips.js
@@ -1,6 +1,6 @@
  // Image manipulation logic from github.com/jwill/psychic-lana
 
-function manipulate(type, r, g, b, a) {
+function manipulate(type) {
   
   var func = function() {};
 
@@ -59,5 +59,5 @@ function manipulate(type, r, g, b, a) {
       break;
   }
 
-  return func(r, g, b, a);
+  return func;
 }
